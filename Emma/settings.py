ac0d392cd758e504a3cfb6bf9cf0499a6052f485
erogui <gautier.travail@gmail.com>
2018-05-06 21:50:02 +0200

    'ff'

--- Emma/settings.py
+++ Emma/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qjsnn9wujrnvo*o)nyvkth*b*l8uf2ibxyjzm7x_!9w9jic8nl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
