67b07d9a6520f32059cfd01a9acc9bb854996b1f
erogui <gautier.travail@gmail.com>
2018-05-06 21:35:00 +0200

    'feger'

--- Emma/settings.py
+++ Emma/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'qjsnn9wujrnvo*o)nyvkth*b*l8uf2ibxyjzm7x_!9w9jic8nl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
